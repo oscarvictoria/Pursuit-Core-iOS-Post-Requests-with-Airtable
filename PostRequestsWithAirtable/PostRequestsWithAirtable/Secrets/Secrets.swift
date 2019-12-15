3fa85f99c3a0d351930556509e4897c5999d98a8
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-12-14 19:43:01 -0500

    got data

---
+++ PostRequestsWithAirtable/PostRequestsWithAirtable/Secrets/Secrets.swift
@@ -0,0 +1,14 @@
+//
+//  Secrets.swift
+//  PostRequestsWithAirtable
+//
+//  Created by Oscar Victoria Gonzalez  on 12/14/19.
+//  Copyright © 2019 Benjamin Stone. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+    static let AirtableAPIKey = "key7ltkf9buarzFxs"
+    static let AirtableProject = "appXX14ZpX4Y3Yn4q"
+}
